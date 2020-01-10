e55496510a4848df4a77de19368ba0a5b1fbbd9a
roo <ruako_takatori@dwango.co.jp>
2020-01-10 21:14:30 +0900

    add 10, 11 chapter

---
+++ 11/kitchen.erl
@@ -0,0 +1,83 @@
+-module(kitchen).
+-compile(export_all).
+
+fridge1() ->
+    receive
+        {From, {store, _Food}} ->
+            From ! {self(), ok},
+            fridge1();
+        {From, {take, _Food}} ->
+            %% uh....
+            From ! {self(), not_found},
+            fridge1();
+        terminate ->
+            ok
+    end.
+
+
+fridge2(FoodList) ->
+    receive
+        {From, {store, Food}} ->
+            From ! {self(), ok},
+            fridge2([Food|FoodList]);
+        {From, {take, Food}} ->
+            case lists:member(Food, FoodList) of
+                true ->
+                    From ! {self(), {ok, Food}},
+                    fridge2(lists:delete(Food, FoodList));
+                false ->
+                    From ! {self(), not_found},
+                    fridge2(FoodList)
+            end;
+        {From, {show, _}} ->
+            From ! {self(), FoodList},
+            fridge2(FoodList);
+        terminate ->
+            ok
+    end.
+
+
+start(FoodList) ->
+    spawn(?MODULE, fridge2, [FoodList]).
+
+store(Pid, Food) ->
+    Pid ! {self(), {store, Food}},
+    receive
+        {Pid, Msg} -> Msg
+    end.
+
+take(Pid, Food) ->
+    Pid ! {self(), {take, Food}},
+    receive
+        {Pid, Msg} -> Msg
+    end.
+
+show_list(Pid) ->
+    Pid ! {self(), {show, list}},
+    receive
+        {Pid, Msg} -> Msg
+    end.
+
+store2(Pid, Food) ->
+    Pid ! {self(), {store, Food}},
+    receive
+        {Pid, Msg} -> Msg
+    after 3000 ->
+            timeout
+    end.
+
+take2(Pid, Food) ->
+    Pid ! {self(), {take, Food}},
+    receive
+        {Pid, Msg} -> Msg
+    after 3000 ->
+            timeout
+    end.
+
+show2_list(Pid) ->
+    Pid ! {self(), {show, list}},
+    receive
+        {Pid, Msg} -> Msg
+    after 3000 ->
+            timeout
+    end.
